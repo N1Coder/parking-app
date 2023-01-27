b6368b476afa10c0cadf44c0e37bc29292ca0360
N1Coder <tomofanteng123@gmail.com>
2023-01-27 08:46:21 +0800

    add changes again

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploy-parking-app/",
+  base: "/parking-app/",
   plugins: [vue()],
   resolve: {
     alias: {
